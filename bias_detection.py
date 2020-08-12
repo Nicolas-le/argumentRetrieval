27a239929f077cebda2a3de24c2b2e9581e56819
Nicolas-le <nicolas.ruth@web.de>
2020-08-12 15:43:36 +0200

    delete test-functionality, clean code

--- bias_detection.py
+++ bias_detection.py
@@ -1,5 +1,5 @@
 from bsdetector import bias
-import csv
+
 
 
 def bias_score(sentence):
@@ -11,23 +11,3 @@ def bias_score(sentence):
 
     return bias.compute_bias(sentence)
 
-
-file = csv.DictReader(open("topics.csv","r"))
-
-for i in file:
-	print(i["Long Query"])
-	print(bias_score(i["Long Query"]))
-
-	print(i["Short Query"])
-	print(bias_score(i["Short Query"]))
-
-	print()
-
-	
-
-	
-
-"""
-sentence = "I hate yellow boats."
-print(bias_score(sentence))
-"""
