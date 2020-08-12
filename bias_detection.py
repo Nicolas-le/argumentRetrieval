95e8f64d6adaf40049daedb4f0393f4d341dc331
Nicolas-le <nicolas.ruth@web.de>
2020-08-12 15:42:15 +0200

    test bias_detection with queries of the course-git

--- bias_detection.py
+++ bias_detection.py
@@ -1,4 +1,5 @@
 from bsdetector import bias
+import csv
 
 
 def bias_score(sentence):
@@ -9,6 +10,22 @@ def bias_score(sentence):
     """
 
     return bias.compute_bias(sentence)
+
+
+file = csv.DictReader(open("topics.csv","r"))
+
+for i in file:
+	print(i["Long Query"])
+	print(bias_score(i["Long Query"]))
+
+	print(i["Short Query"])
+	print(bias_score(i["Short Query"]))
+
+	print()
+
+	
+
+	
 
 """
 sentence = "I hate yellow boats."
