e85f6894c6db5dafaa6bf531494cffc53eeb949f
Nicolas-le <nicolas.ruth@web.de>
2020-08-15 14:05:06 +0200

    Changed the comments

--- bias_detection.py
+++ bias_detection.py
@@ -4,9 +4,9 @@ from bsdetector import bias
 
 def bias_score(sentence):
     """
-    Sent Analysis + bias lex + empath
-    :param tokens:
-    :return:
+    Main function calling the bsdetector.
+    :param sentence: Sentence, the bias should be calculated for.
+    :return: a bias value between -3 and 3
     """
 
     return bias.compute_bias(sentence)
