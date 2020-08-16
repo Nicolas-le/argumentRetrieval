e41eb93f0f7a280f1f61d0a130ea52ff8d0e6352
Nicolas-le <nicolas.ruth@web.de>
2020-08-16 12:38:02 +0200

    flattend the graph

--- hectic.py
+++ hectic.py
@@ -5,8 +5,8 @@ def distance(spelling_errors,average_sentlength):
     Calculates an error value of the wanted linear relation between spelling errors and average sentence length.
     :return: transformed error/distance value
     """
-    # f(x)= -x+10 --> function of which the error value is calculated of, assumed/wanted linear model
-    y = -spelling_errors+10
+    # f(x)= -0.3x+10 --> function of which the error value is calculated of, assumed/wanted linear model
+    y = -(0.3*spelling_errors)+10
 
     #the distance/error is the absolute value between the estimated average sentence length and the measured one
     dist = abs(y-average_sentlength)
