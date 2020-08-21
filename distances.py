a0fa09e568181c18f6e34ce7a3511f3947a74b1a
Nicolas-le <nicolas.ruth@web.de>
2020-08-21 16:32:35 +0200

    update to newest function

--- distances.py
+++ distances.py
@@ -1,4 +1,5 @@
 import numpy
+import hectic
 
 
 def bias_distance(bias_val_one, bias_val_two):
@@ -17,38 +18,50 @@ def bias_distance(bias_val_one, bias_val_two):
 def stylo_distance(stylo_dict_one, stylo_dict_two):
     """
     Creates the vectors out of the dictionaries created by the nlp analysis.
-    returns the euclidean distance between the vectors
-    :param stylo_dict_one:
-    :param stylo_dict_two:
-    :return:
+    returns the euclidean distance between the vSectors including the influence of spelling errors to the average sentence length
+    :param stylo_dict_one:  Calculated dictionary containing stylometric values one
+    :param stylo_dict_two:  Calculated dictionary containing stylometric values two
+    :return:                a distance value of these two dictionaries (space vector model)
     """
     vector_one = create_vector(stylo_dict_one)
     vector_two = create_vector(stylo_dict_two)
 
-    return euclidean_distance(vector_one,vector_two)
+    euclid_dist = euclidean_distance(vector_one,vector_two)
+    spelling_error_influence = hectic.distance(stylo_dict_one["spelling_errors"],stylo_dict_two["readability_measures"]["average_sentlength"])
+
+    if spelling_error_influence == 0:
+        distance = euclid_dist
+    else:
+        distance = euclid_dist*spelling_error_influence
+
+    return distance
 
 
 def create_vector(style_dict):
-    #spelling errors zu länge eher,
-    vector = [0,0,0,0,0]
+    """
+    Creates a vector out of one stylometric dictionary
+    :param style_dict:      stylometric dictionary
+    :return:                an array containing the wanted dimension values --> vector
+    """
+    vector = [0,0,0,0]
 
-    vector[0] = style_dict["vocab_richness"]
+    vector[0] = style_dict["vocab_richeness"]
     vector[1] = style_dict["hepax_legomena"]
     vector[2] = style_dict["readability_measures"]["average_wordlength"]
     vector[3] = style_dict["readability_measures"]["average_sentlength"]
-    vector[4] = style_dict["spelling_errors"]
 
     return vector
 
 def euclidean_distance(vector_one, vector_two):
+    """
+    Calculates the euclidean distance in a space vector model of two vectors.
+    :param vector_one:      first vector
+    :param vector_two:      second vector
+    :return:                multidimensional distance value
+    """
     a = numpy.array(vector_one)
     b = numpy.array(vector_two)
 
     dist = numpy.linalg.norm(a-b)
 
     return dist
-
-"""
-sample_dict_query = {'vocab_richeness': 0.9090909090909091, 'hepax_legomena': 0.8181818181818182, 'readability_measures': {'average_wordlength': 3.727272727272727, 'average_sentlength': 5.5}, 'spelling_errors': 0.0}
-print(stylo_distance(sample_dict,sample_dict))
-"""
