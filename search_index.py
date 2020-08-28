cc3500b351e62d261b5d6172ae9e407b2556d3ed
Abdulnaser98 <64617965+Abdulnaser98@users.noreply.github.com>
2020-08-28 12:51:11 +0200

    Update search_index.py
    modifing the search_index file

--- search_index.py
+++ search_index.py
@@ -1,11 +1,13 @@
 import json
 from elasticsearch import Elasticsearch
 from connect_to_elasticsearch import connect_to_elasticsearch
-from custom_analytics import nlp_analysis
+from nlp_analysis import analyze
 from ranking_results import ranking_results
+from process_results import * 
+from process_trec_format import * 
 
 
-def search_index( es_object, index_name, query ):
+def search_index( es_object, index_name, topic_query ):
     """
     Searches a given index by a given query and presents the retrieved documents ranked by custom nlp scores. 
     :param es_object:   connection to the elasticsearch cluster
@@ -13,21 +15,21 @@ def search_index( es_object, index_name, query ):
     :param query:       the query by which the index will get searched
     :return:            list of retrieved documents ranked by custom nlp scores
     """
-    search_query = multi_match_search( query )
+    search_query = multi_match_search( topic_query )
     search_results = es_object.search( index=index_name, body=search_query, size=1000 )
     print( 'Total of %d documents retrieved\n' % search_results['hits']['total']['value'] )
 
     list_of_results = [ doc for doc in search_results['hits']['hits'] ]
     
-    query_scores = nlp_analysis.analyze( query )
+    results_scors= process_results( list_of_results ) 
+    query_scores = analyze( topic_query )
     search_query['custom_scores'] = query_scores
     with open( 'results/query.json', 'w' ) as file:
-        json.dump( search_query, file, indent=4 )
-
-    ranked_results = ranking_results( list_of_results, query_scores )
+         json.dump( search_query, file, indent=4 )
+    
+    ranked_results = ranking_results( results_scors, query_scores )
     with open( 'results/results.json', 'w' ) as file:
-        json.dump( ranked_results, file, indent=4 )
-
+         json.dump( ranked_results, file, indent=4 )
     return ranked_results
 
 
@@ -51,28 +53,46 @@ def multi_match_search( query ):
     return search_query
 
 
-def search_and_display( es_object, index_name, query ):
+def search_and_display( es_object, index_name, topic_query, topic_number, outputDir ):
     """
     Entry function for an index search. Takes in the input query an displays the search results. 
     :param es_object:   connection to the elasticsearch cluster
     :param index_name:  name of the index which will get searched
     :param query:       the input query by which the index will get searched
     """
-    ranked_results = search_index( es_object, index_name, query )
-
-    print( 'Query: %s' % query )
+    print( 'topic_Query: %s' % topic_query )
+    print( 'topic_number: %s' % topic_number )
+    ranked_results = search_index( es_object, index_name, topic_query )
+    #write_header()
     print( '\nDocuments reranked by the comparisson of bias detection, stylometrics and hidden topics :\n' )
+    rank = 1
     for doc in ranked_results:
+        write_into_trec(doc, topic_number,outputDir,rank )
+        rank = rank + 1 
         print( 'New score: %f' % doc['new_doc_score'] )
         print( 'Original score: %f' % doc['_score'] )
-        print( 'args.me ID: %s' % doc['_source']['argsMeID'] )
         print( 'Title: %s' % doc['_source']['discussionTitle'] )
         print( 'Conclusion: %s' % doc['_source']['conclusion'] )
         print( 'Premise: %s' % doc['_source']['premise'] )
-        print( 'Source: %s\n' % doc['_source']['sourceUrl'] )
+    
 
 
-es_object = connect_to_elasticsearch()
-index_name = 'testindex1'
-query = 'is a strong police force good?'
-search_and_display( es_object, index_name, query )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
