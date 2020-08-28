3bc3e0811c527b85d1847646c9b233b503992804
Abdulnaser98 <64617965+Abdulnaser98@users.noreply.github.com>
2020-08-28 12:44:40 +0200

    Update main.py
    modifing the main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 #import the necessary packages
 import argparse
 
-#from create_index_object import *
+from connect_to_elasticsearch import *
 
-
+from create_index import *
 
 from process_topics import *
 
-import sys 
-print(sys.path)
+from extract_dataSet_toIndex import *
 
+import sys 
+
+from process_topics import * 
+
+
+from process_trec_format import *
 
 
 
@@ -33,20 +38,82 @@ ap.add_argument("-o", "--output-dir", required=True ,help='topics-dir')
 #turn the parsed command line arguments into a Python dictionary 
 args = vars(ap.parse_args())
 
-if os.path.exists(args["output_dir"]):
-       print("File exist")
+if os.path.exists(args["output_dir"]) & os.path.exists(args["input_dir"]) :
+       print("input and outputfiles exist")
 else:
     print("File does not exist!")
 
 
 
-inputDataSet = args["input-dir"]
+inputDataSet = args["input_dir"]
+
 outputDir = args["output_dir"]
 
+
+
+
+
 #elastic_search object
-#es = create_index_object()
+
+es_obj = connect_to_elasticsearch()
 
 
-process_xml(topics_dir)
+#create an Index_object
+
+index_name = "index_v1.0"
+
+#indexObj = create_index( es_obj, index_name )
+
+
+#start extracting and indexing the corpus 
+
+#extractdataSetToIndex( inputDataSet, es_obj, index_name  )
+
+
+#start processing the topics and searching them
+ 
+#write the header of the trec-style run file. 
+write_header( outputDir )
+
+
+
+process_xml( es_obj, index_name,inputDataSet,outputDir )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
