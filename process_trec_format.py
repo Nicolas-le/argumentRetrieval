f10fae525f329b1409c45a53feaa52b7d8af72a0
Abdulnaser98 <64617965+Abdulnaser98@users.noreply.github.com>
2020-08-29 11:03:16 +0200

    Update process_trec_format.py
    changing from csv to txt

--- process_trec_format.py
+++ process_trec_format.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+import os 
 
 
 
@@ -9,24 +10,30 @@ import pandas as pd
 def write_into_trec( doc, topicNumber,outputDir, rank  ):
 
 
-     with open(f'{outputDir}/run.csv', 'a', newline='') as csvfile:
-        fieldnames = ['topic_number', 'Q0', 'arg_ids','rank','score', 'method']
-        writer = csv.DictWriter(csvfile, delimiter =' ',fieldnames=fieldnames)
-        writer.writerow( {'topic_number':topicNumber, 'Q0':'Q0' ,'arg_ids':doc['_source']['id'],'rank':rank ,'score':doc['new_doc_score'], 'method':'bias_detection' } )
-       
+      with open(f'{outputDir}/run.txt', 'a', newline='') as txtfile:
+           txtfile.write( topicNumber+" "+"Q0"+" "+str(doc['_source']['id'])+" "+str(doc['new_doc_score'])+" "+ "bias_detection"+"\n" )
+
 
 
 
 def write_header( outputDir ):
 
-    with open(f'{outputDir}/run.csv', 'a', newline='') as csvfile:
-        fieldnames = ['topic_number', 'Q0', 'arg_ids','rank','score', 'method']
-        writer = csv.DictWriter(csvfile, delimiter =' ',fieldnames=fieldnames)
-        writer.writeheader()
+   with open(f'{outputDir}/run.txt', 'a', newline='') as txtfile:
+        
+        txtfile.write("topic_number,  Q0,  arg_ids,  score,  method "+"\n")
 
 
 
 
+
+def generate_results( output_dir ):
+    
+    text = open(os.path.join(output_dir, "run.csv"), "r")
+    text = ''.join([i for i in text]) \
+        .replace(" ", "    ")
+    x = open(os.path.join(output_dir, "run1.csv"),"w")
+    x.writelines(text)
+    x.close()
 
 
 
