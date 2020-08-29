0482f026a02d501e5cd8f2c0263f2a4c43b39214
Abdulnaser98 <64617965+Abdulnaser98@users.noreply.github.com>
2020-08-29 11:12:31 +0200

    Update process_results.py

--- process_results.py
+++ process_results.py
@@ -8,9 +8,9 @@ def process_results( results_list ):
         scores= analyze( result['_source']['premise'] )
         new_dic = dict( result, **scores )
 
-        for keys,values in new_dic.items():
-            print(keys)
-            print(values)
+       # for keys,values in new_dic.items():
+        #    print(keys)
+         #   print(values)
         rl.append( new_dic )
         count= count+1 
         print(count)
