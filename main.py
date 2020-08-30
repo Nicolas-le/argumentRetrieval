278c637c6b96d21e19630c63099a9d5c31b8a2b1
Nicolas-le <nicolas.ruth@web.de>
2020-08-31 01:39:46 +0200

    code styling

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 #import the necessary packages
 import argparse
-
 import sys
 sys.path.append('./code_base/nlp_analytics')
 sys.path.append('./code_base/ES')
 sys.path.append('./code_base/distance_calculations')
 
 from connect_to_elasticsearch import *
-
 from create_index import *
-
 from process_topics import *
-
 from extract_dataSet_toIndex import *
-
-
-
 from process_topics import * 
-
-
 from process_trec_format import *
-
 import csv
-
-
-
-
-
 #construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser(description='Index the corus and generates runs!')
 
@@ -40,8 +25,6 @@ ap.add_argument("-i", "--input-dir" , required=True ,help='corpus-dir')
 We will create a standard trec run file in $outputDir/run.txt
 """
 ap.add_argument("-o", "--output-dir", required=True ,help='topics-dir')
-
-
 #turn the parsed command line arguments into a Python dictionary 
 args = vars(ap.parse_args())
 
@@ -49,21 +32,13 @@ if os.path.exists(args["output_dir"]) & os.path.exists(args["input_dir"]) :
        print("input and outputfiles exist")
 else:
     print("File does not exist!")
-
-
-
 inputDataSet = args["input_dir"]
 
 outputDir = args["output_dir"]
 
-
-
-
-
 #elastic_search object
 
 es_obj = connect_to_elasticsearch()
-
 
 #create an Index_object
 
@@ -71,39 +46,9 @@ index_name = "index_v1.0"
 
 indexObj = create_index( es_obj, index_name )
 
-
 #start extracting and indexing the corpus 
-
 extractdataSetToIndex( inputDataSet, es_obj, index_name  )
-
-
 
 #processing the xml_topics and searching them 
 process_xml( es_obj, index_name,inputDataSet,outputDir,True )
 process_xml( es_obj, index_name,inputDataSet,outputDir,False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
