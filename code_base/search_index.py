a4ecdad9961e2b5d6f1226279d7fc18c137c8f85
Abdulnaser98 <64617965+Abdulnaser98@users.noreply.github.com>
2020-08-31 00:18:52 +0200

    Update search_index.py
    add a bool condition to retrieve the args based on the pro/con

--- code_base/search_index.py
+++ code_base/search_index.py
@@ -7,101 +7,102 @@ from process_results import *
 from process_trec_format import * 
 
 
-# for multi-field full text search with the query analyzed in the same way as the idexed documents got analyzed
-def multi_match_search( query ):
-    """
-    Defining the search process e.g. which fields should be searched and how acurate the query should match.
-    :param query:   input search query
-    :return:        dictionary which helds a defined search query for processing an index search
-    """
-    search_query = {
-        'query': {
-            'multi_match': {
-                'query' : query,
-                'fields' : [ 'conclusion', 'premise', 'discussionTitle' ],
-                'fuzziness' : 'AUTO',
-                'tie_breaker' : 0.3
-            }
-        }
-    }
-    return search_query
-
-
-def search_index( es_object, index_name, query, search_nr ):
+def search_index( es_object, index_name, topic_query,value ):
     """
     Searches a given index by a given query and presents the retrieved documents ranked by custom nlp scores. 
     :param es_object:   connection to the elasticsearch cluster
     :param index_name:  name of the index which will get searched
     :param query:       the query by which the index will get searched
-    :param search_nr:   number of the attempted search for evaluation file
     :return:            list of retrieved documents ranked by custom nlp scores
     """
-    search_query = multi_match_search( query )
-    search_results = es_object.search( index=index_name, body=search_query, size=100  )
+    search_query = multi_match_search( topic_query,value )
+    search_results = es_object.search( index=index_name, body=search_query, size=50,request_timeout=300 )
     print( 'Total of %d documents retrieved\n' % search_results['hits']['total']['value'] )
-   
+
     list_of_results = [ doc for doc in search_results['hits']['hits'] ]
-    with open( 'results/ranking_BM25' + '_topic' + str(search_nr) + '.json', 'w' ) as file:
-        json.dump( list_of_results, file, indent=4 )
-
-    results_custom_ranking = customize_ranking( list_of_results, query, search_nr )
     
-    return results_custom_ranking
-
-
-def customize_ranking( list_of_results, query, search_nr ):
-    """
-    Searches a given index by a given query and presents the retrieved documents ranked by custom nlp scores. 
-    :param list_of_results:     list of retrieved documents ranked inside Elastic Search with BM25
-    :param query:               the query by which the index will get searched
-    :param search_nr:           number of the attempted search for evaluation files
-    :return:                    list of retrieved documents ranked by custom nlp scores
-    """
-    print('analyzing query...')
-    query_scores = analyze( query )
-    es_query = multi_match_search( query )
-    es_query['nlp_scores'] = query_scores
-    print('output query')
-    with open( 'results/query' + '_topic' + str(search_nr) + '.json', 'w' ) as file:
-        json.dump( es_query, file, indent=4 )
-
-    print('analyzing retrieved documents...')
-    for doc in list_of_results:
-        nlp_scores = analyze( doc['_source']['premise'] )
-        doc['nlp_scores'] = nlp_scores
-
-    print('ranking retrieved documents...')
-    results_custom_ranking = ranking_results( list_of_results, query_scores )
-    print('output custom ranked documents')
-    with open( 'results/results_custom' + '_topic' + str(search_nr) + '.json', 'w' ) as file:
-        json.dump( results_custom_ranking, file, indent=4 )
+    results_scors= process_results( list_of_results ) 
+    query_scores = analyze( topic_query )
+    search_query['custom_scores'] = query_scores
+    with open( 'results/query.json', 'w' ) as file:
+          json.dump( search_query, file, indent=4 )
     
-    return results_custom_ranking
+    ranked_results = ranking_results( results_scors, query_scores )
+    with open( 'results/results.json', 'w' ) as file:
+          json.dump( ranked_results, file, indent=4 )
+    return list_of_results
 
 
-def search_and_display( es_object, index_name, topic_query, topic_number, outputDir ):
+# for multi-field full text search with the query analyzed in the same way as the idexed documents got analyzed
+def multi_match_search( query, value ):
+    """
+    Defining the search process e.g. which fields should be searched and how acurate the query should match.
+    :param query:   input search query
+    :return:        dictionary which helds a defined search query for processing an index search
+    """
+    #search_query = {
+        # 'query': {
+           #  'multi_match': {
+          #       'query' : query,
+         #        'fields' : [ 'conclusion', 'premise', 'discussionTitle' ],
+        #         'fuzziness' : 'AUTO',
+       #          'tie_breaker' : 0.3
+      #       }
+     #    }
+    # }
+    
+     
+    search_query = {
+           'query': {
+               'bool': {
+                  'must': {
+                     'multi_match': {
+                                       'fields': [
+                                                       'conclusion',
+                                                       'premise',
+                                                       'discussionTitle',
+                                                 ],
+                                       'query' : query,
+                                       'fuzziness' : 'AUTO',
+                                       'tie_breaker' : 0.3
+
+
+ 
+                                    }
+               },
+               'filter': {
+                   'terms': {
+                        'stance': [value] 
+                   }
+               }
+          }
+       }
+   }
+
+
+
+
+    return search_query
+
+
+def search_and_display( es_object, index_name, topic_query, topic_number, outputDir, value ):
     """
     Entry function for an index search. Takes in the input query an displays the search results. 
-    :param es_object:       connection to the elasticsearch cluster
-    :param index_name:      name of the index which will get searched
-    :param topic_query:     the input query by which the index will get searched
-    :param topic_number:    number of the attempted search for evaluation files
-    :param outputDir:       directory for output of trec record
+    :param es_object:   connection to the elasticsearch cluster
+    :param index_name:  name of the index which will get searched
+    :param query:       the input query by which the index will get searched
     """
-    print( 'topic_query: %s' % topic_query )
-    print( 'topic_number: %s' % topic_number )
-    ranked_results = search_index( es_object, index_name, topic_query, topic_number )
-    #write_header()
+    ranked_results = search_index( es_object, index_name, topic_query,value )
     print( '\nDocuments reranked by the comparisson of bias detection, stylometrics and hidden topics :\n' )
-    rank = 1
-    for doc in ranked_results:
-        write_into_trec(doc, topic_number, outputDir, rank )
-        rank = rank + 1 
-        print( 'New score: %f' % doc['new_score'] )
-        print( 'Original score: %f' % doc['_score'] )
-        print( 'Title: %s' % doc['_source']['discussionTitle'] )
-        print( 'Conclusion: %s' % doc['_source']['conclusion'] )
-        print( 'Premise: %s' % doc['_source']['premise'] )
+    print("topic_number,  Q0,  arg_ids,  score,  method "+"\n")
+    #for doc in ranked_results:
+    write_into_trec(ranked_results, topic_number,outputDir )
+    
+      #  print( 'New score: %f' % doc['new_doc_score'] )
+      #  print( 'Original score: %f' % doc['_score'] )
+      #  print( 'Title: %s' % doc['_source']['discussionTitle'] )
+      #  print( 'Conclusion: %s' % doc['_source']['conclusion'] )
+      #  print( 'Premise: %s' % doc['_source']['premise'] )
     
 
 
