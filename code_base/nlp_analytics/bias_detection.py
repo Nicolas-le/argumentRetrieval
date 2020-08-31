ff4dcf2e28954d4429ad7858c6aa6199b9ab677f
Nicolas-le <nicolas.ruth@web.de>
2020-08-31 13:08:51 +0200

    solve problem with long input sources, normalize

--- code_base/nlp_analytics/bias_detection.py
+++ code_base/nlp_analytics/bias_detection.py
@@ -1,13 +1,18 @@
 from bsdetector import bias
+import nltk
 
 
 
-def bias_score(sentence):
+def bias_score(text):
     """
     Main function calling the bsdetector.
     :param sentence: Sentence, the bias should be calculated for.
-    :return: a bias value between -3 and 3
+    :return: a bias value between 0 and 3
     """
+    sentences = nltk.sent_tokenizer(text)
+    total_bias = 0
+    for sentence in sentences:
+        total_bias += bias.compute_bias(sentence)
 
-    return bias.compute_bias(sentence)
+    return total_bias/len(sentences)
 
