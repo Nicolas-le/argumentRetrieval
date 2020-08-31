d253afa99ca642a586b477f64de6826c641e9cee
Abdulnaser98 <64617965+Abdulnaser98@users.noreply.github.com>
2020-08-31 13:16:24 +0200

    Update process_results.py

--- code_base/ES/process_results.py
+++ code_base/ES/process_results.py
@@ -5,9 +5,7 @@ processes the given results_list , which is a list of dictionaries given back by
 then we process each dictionary and extract the premise out of it in order to be able to analyze it 
 and then we append the scores dictionary to the original dictionary and in this way we could analyse each 
 retrieved argument based on out approach and add append the resulted values with the orginal result.
-
 """
-
 def process_results( results_list ):
     rl = []
     count = 0
