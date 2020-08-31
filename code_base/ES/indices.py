3da7a692e1e10065c711f51e27a07846394a3ac1
Abdulnaser98 <64617965+Abdulnaser98@users.noreply.github.com>
2020-08-31 10:10:14 +0200

    Update indices.py
    Adding a comment to the function

--- code_base/ES/indices.py
+++ code_base/ES/indices.py
@@ -1,6 +1,6 @@
 from connect_to_elasticsearch import *
 
-
+# returns the name of all indices in the elasticsearch server 
 def getAllIndiciesNames():  
     indicies = set()
     for index in connect_to_elasticsearch().indices.get_alias( "*" ):
