eef30f036d622a9a02332c9fe43fc1a644235632
Abdulnaser98 <64617965+Abdulnaser98@users.noreply.github.com>
2020-08-31 11:49:19 +0200

    Update process_results.py
    Adding some comments to explain the functionality of the process_results

--- code_base/ES/process_results.py
+++ code_base/ES/process_results.py
@@ -1,5 +1,12 @@
 from nlp_analysis import *
 
+"""
+processes the given results_list , which is a list of dictionaries given back by the search function 
+then we process each dictionary and extract the premise out of it in order to be able to analyze it 
+and then we append the scores dictionary to the original dictionary and in this way we could analyse each 
+retrieved argument based on out approach and add append the resulted values with the orginal result.
+
+"""
 
 def process_results( results_list ):
     rl = []
